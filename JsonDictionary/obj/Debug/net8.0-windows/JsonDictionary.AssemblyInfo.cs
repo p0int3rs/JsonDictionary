ef41fc2d088ef2e49d8e55d6385f50946bbd661a
Shinotlly <birisi_birisi_sam@outlook.com>
2026-02-23 15:52:54 +0300

    amantamrim
    tamrimaman

--- JsonDictionary/obj/Debug/net8.0-windows/JsonDictionary.AssemblyInfo.cs
+++ JsonDictionary/obj/Debug/net8.0-windows/JsonDictionary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JsonDictionary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3a741c88c461ce05db7ee298a5d17bc32e40d8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eb7491534cf5d46c581bc0e863b3d26c90974e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("JsonDictionary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JsonDictionary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
